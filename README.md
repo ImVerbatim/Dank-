bfae5794159de8c4c2257fdc5d25e87bfd5690c1
imverbatim <ajdelco22@gmail.com>
2019-05-19 17:42:13 +0000

    Implemented sourceFile, sourceFileUtils functions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Dank-
-A dank programming language based off of C++ 
+# Dank++
+A dank programming language based off of C++. This languages is designed purely form experimental and learning purposes and has no intention of being used for development. 
---
+++ h/SourceFile.h
@@ -0,0 +1,28 @@
+#ifndef SOURCEFILE_H
+#define SOURCEFILE_H
+
+#include <string>
+
+class SourceFile
+{
+    public:
+        SourceFile() {
+            fileName = "[empty_file]";
+            contents = "";
+        }
+
+        SourceFile(std::string,bool);
+
+        std::string getDirPath();
+        std::string getFileName();
+        std::string getLine(int);
+
+        inline int getSizeOfContent() { return contents.size(); }
+    
+    private:
+        std::string fileName;
+        std::string contents;
+};
+
+
+#endif
